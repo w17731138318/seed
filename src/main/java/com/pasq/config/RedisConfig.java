82b7db54ff2798863f3b513c346ffd78c37a4599
wwx <w17731138318@qq.com>
2017-12-27 09:28:10 +0800

    修改redis序列化方式

--- src/main/java/com/pasq/config/RedisConfig.java
+++ src/main/java/com/pasq/config/RedisConfig.java
@@ -1,13 +1,10 @@
 package com.pasq.config;
 
-import com.pasq.common.utils.ProtoSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -24,25 +21,14 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig extends CachingConfigurerSupport {
     @Autowired
     private RedisConnectionFactory factory;
-    @Bean
-    public CacheManager cacheManager(RedisTemplate redisTemplate) {
-        RedisCacheManager rcm = new RedisCacheManager(redisTemplate);
-       /* //设置缓存过期时间
-        // rcm.setDefaultExpiration(60);//秒
-        //设置value的过期时间
-        Map<String,Long> map=new HashMap();
-        map.put("test",60L);
-        rcm.setExpires(map);*/
-        return rcm;
-    }
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        ProtoSerializer protoSerializer = new ProtoSerializer();
-        redisTemplate.setKeySerializer(protoSerializer);
-        redisTemplate.setHashKeySerializer(protoSerializer);
-        redisTemplate.setHashValueSerializer(protoSerializer);
-        redisTemplate.setValueSerializer(protoSerializer);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(factory);
         return redisTemplate;
     }
